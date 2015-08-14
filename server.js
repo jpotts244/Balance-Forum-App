888b4762f4308e809d194f53b4711597dcf52423
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2015-08-13 23:08:03 -0400

    finished categories and posts CRUD

--- server.js
+++ server.js
@@ -29,18 +29,17 @@ app.get("/", function (req, res){
 	
 });
 
-
 app.get("/categories/:id/posts", function (req, res){
 	var id = req.params.id;
 
-	db.get("SELECT * FROM categories WHERE id = ?", id, function(err, row){
+	db.get("SELECT * FROM categories WHERE id=?", id, function(err, row){
 		var category = row;
-		db.all("SELECT * FROM posts INNER JOIN categories ON posts.categories_id = categories.id WHERE posts.categories_id=?", id, function (err, row){
+		db.all("SELECT * FROM categories INNER JOIN posts ON posts.categories_id = categories.id WHERE posts.categories_id=?", id, function (err, row){
 			if(err){
 				console.log(err);
 			} else {
 				console.log(row);
-				var template = fs.readFileSync("./views/categoriesShow.html", "utf8");
+				var template = fs.readFileSync("./views/categories_show.html", "utf8");
 				var rendered = ejs.render(template, {posts: row, category: category});
 				res.send(rendered);
 			}
@@ -49,35 +48,76 @@ app.get("/categories/:id/posts", function (req, res){
 
 })
 
-
 app.get("/categories/:id/posts/new", function (req, res){
-	var newPostForm = fs.readFileSync("./views/categoriesShow.html", "utf8");
+	var newPostForm = fs.readFileSync("./views/categories_show.html", "utf8");
 	res.send(newPostForm);
 });
 
 
-app.get("/categories/:catId/posts/:postsId", function (req, res){
-	var catId = req.params.catId;
-	var postsId = req.params.postsId;
-	console.log(catId);
-	console.log(postsId);
+// app.get("/categories/:catId/posts/:postId", function (req, res){
+// 	var catId = req.params.catId;
+// 	var postId = req.params.postId;
+// 	db.get("SELECT * FROM categories WHERE id=?", catId, function (err, row){
+// 		var category = row;
 	
-	db.get("SELECT posts.title, posts.content, posts.author, posts.rating, categories.type FROM posts INNER JOIN categories ON posts.categories_id = categories.id WHERE categories.id=? AND posts.id=?", catId, postsId, function (err, row){
+// 	db.get("SELECT * FROM posts WHERE posts.id=? AND posts.categories_id=?", postId, catId, function (err, row){
+// 		if (err){
+// 			console.log(err);	
+// 		} else {
+// 			console.log(row);
+// 			var template = fs.readFileSync("./views/show_post.html", "utf8");
+// 			var html = ejs.render(template, {post: row, category: category});
+// 			res.send(html);
+// 		}
+// 	})
+// 	})
+// })
+
+app.get("/categories/:catId/posts/:postId", function (req, res){
+	var catId = req.params.catId;
+	var postId = req.params.postId;
+	
+	db.get("SELECT * FROM categories INNER JOIN posts ON posts.categories_id WHERE posts.id=? AND posts.categories_id=?", postId, catId, function (err, row){
 		if (err){
 			console.log(err);	
 		} else {
 			console.log(row);
-			// var template = fs.readFileSync("./views/show-post.html", "utf8");
-			// var html = ejs.render(template, {post: row});
-			// res.send(html);
+			var template = fs.readFileSync("./views/show_post.html", "utf8");
+			var html = ejs.render(template, {post: row});
+			res.send(html);
 		}
 	})
 })
 
-// app.post("/categories/posts", function (req, res){
-// 	var newPost = req.body;
+app.post("/categories/:id/posts", function (req, res){
+	var id = req.params.id;
+	var newPost = req.body;
+	db.run("INSERT INTO posts (title, content, author, rating, categories_id) VALUES (?,?,?,?,?)", newPost.title, newPost.content, newPost.author, 0, id, function (err){
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect("/");
+		}
+	});
 
-// })
+});
+
+////FIX MEEEEE
+app.get("/categories/:catId/posts/:postId/comments", function (req, res){
+	var catId = req.params.catId;
+	var postId = req.params.postId;
+
+	db.get("SELECT * FROM posts INNER JOIN comments ON comments.posts_id = posts.id WHERE posts.categories_id=? AND posts.id=?", catId, postId, function (req, res){
+		if (err){
+			console.log(err);
+		} else {
+			console.log(row);
+			var template = fs.readFileSync("./views/show_post.html", "utf8");
+			var html = ejs.render(template, {comments: row});
+			res.send(html);
+		}
+	})
+})
 
 
 
