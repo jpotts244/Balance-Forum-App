bd80b57102192356e3293b22e734809dba3e5a95
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2015-08-15 23:02:29 -0400

    adds font buttons

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 app.use(express.static('images'));
+app.use(express.static('fonts'));
 var fs = require('fs');
 var ejs = require('ejs');
 
